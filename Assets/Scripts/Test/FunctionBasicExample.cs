27ab56a4080018e26840b6bc9107e5a5840a7b02
JJH0204 <>
2026-03-24 19:46:22 +0900

    .

---
+++ Assets/Scripts/Test/FunctionBasicExample.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+
+public class FunctionBasicExample : MonoBehaviour
+{
+    int playerHp = 5;
+    int playerScore = 0;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        // 3개의 함수를 차례대로 호출해 보시오.
+        //PrintLessonStartMessage();
+        //PrintPlayerName();
+        //PrintLessonEndMessage();
+        //AddScore(100);
+    }
+
+    private void PrintLessonStartMessage()
+    {
+        Debug.Log("함수 수업 예제를 시작합니다.");
+    }
+
+    private void PrintPlayerName()
+    {
+        Debug.Log("플레이어의 이름은 Hero입니다.");
+    }
+
+    private void PrintLessonEndMessage()
+    {
+        Debug.Log("함수 수업 예제를 마칩니다.");
+    }
+
+    private void TakeDamage(int damage)
+    {
+        playerHp -= damage;
+        Debug.Log("데미지를 입었습니다 : " + damage);
+        Debug.Log("현재 체력 : " + playerHp);
+    }
+
+    // 점수를 매개변수로 받고, playerScore에 매개변수로 받은 점수를 합산하고, 현재 점수를 Debug.Log 출력하는 함수 선언.
+    private void AddScore(int score)
+    {
+        playerScore += score;
+        Debug.Log("현재 점수 : " + playerScore);
+    }
+
+    // playerHp가 0 이하인지 체크해서 결과를 반환하는 함수를 선언. 결과는 bool 값을 반환.
+    // TakeDamage 함수 호출 playerHp 감소. 감소량은 각자 알아서.
+    // playerHp가 0 이하인지 체크해서 결과를 반환하는 함수를 호출하고 결과를 bool 형 변수에 저장.
+    // if 문으로 결과가 true인지 false인지 검사.
+    // true면 "플레이어가 사망했습니다" 메시지 출력. false면 "아직 싸울 수 있다" 메시지 출력.
+}
--- Assets/Scripts/Test/LoopGameSimulation.cs
+++ Assets/Scripts/Test/LoopGameSimulation.cs
@@ -5,38 +5,55 @@ public class LoopGameSimulation : MonoBehaviour
     int enemySpawnCount = 5;
     int coinCount = 3;
     int startWave = 3;
+    int playerHp = 5;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        for(int i=0; i<enemySpawnCount; i++)
-        {
-            int enemyNumber = i + 1;
-            Debug.Log("적 생성 완료: " + enemyNumber + "번째 적");
-        }
+        //for(int i=0; i<enemySpawnCount; i++)
+        //{
+        //    int enemyNumber = i + 1;
+        //    Debug.Log("적 생성 완료: " + enemyNumber + "번째 적");
+        //}
 
-        Debug.Log("적 생성 처리 완료");
+        //Debug.Log("적 생성 처리 완료");
 
-        for(int i=0; i<coinCount; i++)
-        {
-            int coinNumber = i + 1;
-            Debug.Log("코인 확인 완료: " + coinNumber + "번째 코인");
-        }
+        //for(int i=0; i<coinCount; i++)
+        //{
+        //    int coinNumber = i + 1;
+        //    Debug.Log("코인 확인 완료: " + coinNumber + "번째 코인");
+        //}
 
-        Debug.Log("모든 코인 확인 완료");
+        //Debug.Log("모든 코인 확인 완료");
 
-        while(startWave > 0)
-        {
-            Debug.Log("현재 웨이브: " + startWave);
-            startWave--;
-        }
+        //while(startWave > 0)
+        //{
+        //    Debug.Log("현재 웨이브: " + startWave);
+        //    startWave--;
+        //}
 
-        Debug.Log("모든 웨이브 진행 완료");
+        //Debug.Log("모든 웨이브 진행 완료");
+
+        // 함수 호출.
+        PrintHello();
+
+        int damage = 10;
+        TakeDamage(damage);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void PrintHello()
+    {
+        Debug.Log("안녕하세요.");
+    }
+
+    private void TakeDamage(int damage)
+    {
+        playerHp -= damage;
     }
 }
