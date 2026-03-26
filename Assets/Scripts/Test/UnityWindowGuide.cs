b2c88107164780736a630d8a5b9189d870b44cb6
JJH0204 <>
2026-03-26 19:52:26 +0900

    .

---
+++ Assets/Scripts/Test/UnityWindowGuide.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class UnityWindowGuide : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("Hierarchy : 현재 씬 안의 오브젝트 목록 출력");
+    }
+}
